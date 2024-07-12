82709411fe2629619fe29d4be44059327f6d91e6
Jorge <fervirlacrer@gmail.com>
2024-07-12 19:35:44 -0400

    últimos detalles responsive

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -53,12 +53,7 @@ const Carousel = () => {
       {/* Header para responsive */}
       <header className="md:hidden fixed top-0 left-0 right-0 bg-white w-full z-20 py-4 flex justify-between items-center px-4">
         <div className="flex items-center">
-          <img
-            src="../../img/alamo-blanco.png"
-            className="w-12 h-12"
-            alt="Logo de la institución"
-          />
-          <span className="text-xl font-bold ml-2">U.E.N JOSE ANGEL ALAMO</span>
+          <span className="text-xl font-bold ">U.E.N JOSE ANGEL ALAMO</span>
         </div>
         <div onClick={() => setOpen(!open)} className="text-3xl cursor-pointer">
           {open ? <HiX /> : <HiOutlineMenu />}
